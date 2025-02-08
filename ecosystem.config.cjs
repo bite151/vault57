f4707bd74e198da5f70c3a9b87306296a5b88639
Dmitry Lebedev <bite151@yandex.ru>
2025-02-08 03:04:36 +0300

    fix: pm2 config

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,11 +1,12 @@
 module.exports = {
     apps: [
         {
-            name: 'Vault57-DevFront',
+            name: 'Vault57DevFront',
             port: '8841',
             exec_mode: 'cluster',
             instances: 'max',
-            script: './.output/server/index.mjs'
+            script: '.output/server/index.mjs',
+            args: 'node PORT=8841 HOST=92.63.100.179'
         }
     ]
 }
